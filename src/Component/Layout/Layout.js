15cdc786353c007e498474b48f52546311985576
Abhishekgupta011 <ag25061999@gmail.com>
2023-11-03 21:44:51 +0530

    Login screen

---
+++ src/Component/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Layout = () =>{
+    return(
+        <>
+        <h1>Welcome to Expense Tracker</h1>
+        </>
+    )
+}
+export default Layout;
--- src/Component/Signup/Signup.js
+++ src/Component/Signup/Signup.js
@@ -1,74 +1,84 @@
 import React, { useState } from "react";
 import './Signup.css'
+import Layout from "../Layout/Layout";
+
 const SignUp = () => {
-    const [email , setEmail] = useState("");
-    const [password , setPassword] = useState("");
-    const [cPassword , setCpassword] = useState("");
-    const [isLogin , setLogin] = useState(false)
-    const emailInputHandler = (event)=>{
-        setEmail(event.target.value)
-    }
-    const passwordInputHandler = (event)=>{
-        setPassword(event.target.value)
-    }
-    const cPasswordInputHandler = (event)=>{
-        setCpassword(event.target.value)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [cPassword, setCpassword] = useState("");
+    const [isLogin, setLogin] = useState(false);
+    const [isLoggedIn, setLoggedIn] = useState(false); // Track the login state
+
+    const emailInputHandler = (event) => {
+        setEmail(event.target.value);
     }
 
-    let formSubmitHandler= async(event)=>{
+    const passwordInputHandler = (event) => {
+        setPassword(event.target.value);
+    }
+
+    const cPasswordInputHandler = (event) => {
+        setCpassword(event.target.value);
+    }
+
+    let formSubmitHandler = async (event) => {
         event.preventDefault();
-        if (password !== cPassword) {
+        if (!isLogin && password !== cPassword) {
             alert("Passwords do not match");
             return;
-          } else{
-            try{
-                const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:${isLogin ? 'signInWithPassword' : 'signUp'}?key=AIzaSyDJk2qCxLvy8gpH7j8NlZsL7Zg0QeB6ZVA`,{
+        } else {
+            try {
+                const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:${isLogin ? 'signInWithPassword' : 'signUp'}?key=AIzaSyDJk2qCxLvy8gpH7j8NlZsL7Zg0QeB6ZVA`, {
                     method: 'POST',
-                    headers:{
-                        "Content-Type" : 'application/json'
+                    headers: {
+                        "Content-Type": 'application/json'
                     },
-                    body:JSON.stringify({
+                    body: JSON.stringify({
                         email,
                         password,
                         returnSecureToken: true,
                     }),
                 })
                 const responseData = await response.json()
-                if(response.ok){
-                    alert(`${isLogin ? 'Login' : 'Sign-Up'} successful`, 'success')
-                    console.log(responseData)
+                if (response.ok) {
+                    alert(`${isLogin ? 'Login' : 'Sign-Up'} successful`, 'success');
+                    console.log(responseData);
+
+                    if(isLogin){
+                        setLoggedIn(true);
+                    }
+                    
                 } else {
                     alert(responseData.error.message || 'Authentication failed');
-
-                  }
-            } catch(error){
+                }
+            } catch (error) {
                 console.error('An error occurred during authentication', error);
             }
-          }
-        
+        }
     }
- 
-    
-    return(
-        
-        <form onSubmit={formSubmitHandler}>
-            <div className="form-container">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" value={email} onChange={emailInputHandler} required placeholder="User Email"/>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" value={password} onChange={passwordInputHandler} required placeholder="Password"/>
-            {!isLogin && <label htmlFor="cpassword">Confirm Password</label>}
-            {!isLogin &&<input type="password" id="cpassword" value={cPassword} onChange={cPasswordInputHandler} required placeholder="Confirm Password"/>}
-            <button type="submit">{isLogin ? "Login" : "SignUp" }</button>
-            </div>
-            <div className="toggle">
-            <button type="button"
-            onClick={() => setLogin(!isLogin)}
-            className="toggle-button"
-            >{isLogin ? "Create New Account? SignUp" : "Have an account? Login"}</button>
-            </div>
-        </form>
-        
+
+    return (
+        <>
+            {isLoggedIn ? <Layout />:<form onSubmit={formSubmitHandler}>
+                <div className="form-container">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" value={email} onChange={emailInputHandler} required placeholder="User Email" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" value={password} onChange={passwordInputHandler} required placeholder="Password" />
+                    {!isLogin && <label htmlFor="cpassword">Confirm Password</label>}
+                    {!isLogin && <input type="password" id="cpassword" value={cPassword} onChange={cPasswordInputHandler} required placeholder="Confirm Password" />}
+                    <button type="submit">{isLogin ? "Login" : "Sign-Up"}</button>
+                </div>
+                <div className="toggle">
+                    <button type="button"
+                        onClick={() => setLogin(!isLogin)}
+                        className="toggle-button"
+                    >{isLogin ? "Create New Account? Sign Up" : "Have an account? Login"}</button>
+                </div>
+            </form>}
+            
+        </>
     )
 }
+
 export default SignUp;
