66e8fdc32d3fe52c998db7879dd518e7da5eb928
Abhishekgupta011 <ag25061999@gmail.com>
2023-11-05 16:59:12 +0530

    edit

--- src/Component/Profile/ProfilePage.js
+++ src/Component/Profile/ProfilePage.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './ProfilePage.css';
+
 const ProfilePage = () => {
     const [username, setUsername] = useState("");
-    const [photoUrl, setPhotoUrl] = useState(""); // Added state for profile photo URL
+    const [photoUrl, setPhotoUrl] = useState("");
+    const [loading, setLoading] = useState(true); // Added loading state
 
     const usernameHandler = (event) => {
         setUsername(event.target.value);
@@ -12,31 +14,65 @@ const ProfilePage = () => {
         setPhotoUrl(event.target.value);
     }
 
-    const updateProfileApi = async (event) => { 
-        event.preventDefault(); 
-        const storedToken = localStorage.getItem("IdToken")
+    const updateProfileApi = async (event) => {
+        event.preventDefault();
+        const storedToken = localStorage.getItem("IdToken");
         try {
-            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDJk2qCxLvy8gpH7j8NlZsL7Zg0QeB6ZVA', {
-                method: "POST",
-                headers: {
-                    "Content-Type": 'application/json'
-                },
-                body: JSON.stringify({
-                    idToken: storedToken,
-                    displayName: username, 
-                    photoUrl: photoUrl, 
-                    returnSecureToken: true,
-                })
-            });
+            const response = await fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDJk2qCxLvy8gpH7j8NlZsL7Zg0QeB6ZVA',
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": 'application/json'
+                    },
+                    body: JSON.stringify({
+                        idToken: storedToken,
+                        displayName: username,
+                        photoUrl: photoUrl,
+                        returnSecureToken: true,
+                    })
+                }
+            );
 
             if (response.ok) {
-                alert("Profile updated successfully")
+                alert("Profile updated successfully");
             } else {
                 // Handle error responses
             }
         } catch (error) {
             console.error('An error occurred while updating the profile', error);
         }
+    }
+
+    useEffect(() => {
+        // Fetch user data and update the state
+        const storedToken = localStorage.getItem("IdToken");
+        if (storedToken) {
+            fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDJk2qCxLvy8gpH7j8NlZsL7Zg0QeB6ZVA', {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data) {
+                        setUsername(data.username || ""); // Update with actual data field name
+                        setPhotoUrl(data.photoUrl || ""); // Update with actual data field name
+                    }
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error('Error fetching user data', error);
+                    setLoading(false);
+                });
+        } else {
+            setLoading(false);
+        }
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
     }
 
     return (
@@ -46,11 +82,11 @@ const ProfilePage = () => {
                 <button>Cancel</button>
             </div>
             <div className="u-form">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" value={username} onChange={usernameHandler} />
-            <label htmlFor="photourl">Profile photo Url</label>
-            <input type="url" id="photourl" value={photoUrl} onChange={photoUrlHandler} />
-            <button type="submit" className="update">Update</button>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" value={username} onChange={usernameHandler} />
+                <label htmlFor="photourl">Profile photo Url</label>
+                <input type="url" id="photourl" value={photoUrl} onChange={photoUrlHandler} />
+                <button type="submit" className="update">Update</button>
             </div>
         </form>
     );
