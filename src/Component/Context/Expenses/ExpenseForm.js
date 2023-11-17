0428f5dcae1df80db95cde3cf9de40c375b55898
Abhishekgupta011 <ag25061999@gmail.com>
2023-11-17 12:15:11 +0530

    integrated

--- src/Component/Context/Expenses/ExpenseForm.js
+++ src/Component/Context/Expenses/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ExpenseList from "./ExpenseList";
 import './ExpenseForm.css'
 
@@ -7,7 +7,7 @@ const ExpenseForm = ()=>{
     const [amount , setAmount] = useState('');
     const [category , setCategory] = useState("");
     const [products, setProducts] = useState([]);
-
+    const [id , setNextId] = useState(1)
     const descriptionHandler = (event)=>{
         setDescription(event.target.value)
     }
@@ -17,24 +17,72 @@ const ExpenseForm = ()=>{
     const categoryHandler = (event)=>{
         setCategory(event.target.value)
     }
-    const ExpenseProductListHandler = (event)=>{
+    const getExpenses = async () => {
+        try {
+            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json",{
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                  },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log("data" ,data)
+                const expensesArray = Object.values(data);
+                console.log("expenses", expensesArray)
+                setProducts(expensesArray);
+            }
+        } catch (error) {
+            console.error('Error fetching expenses:', error);
+        }
+    }
+    const ExpenseProductListHandler = async(event)=>{
         event.preventDefault();
         const product = {
+            id: id,
             description,
             amount,
             category,
         }
-        setProducts([...products, product]);
+        console.log(id)
+        
+        try{
+            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json",{
+                method: 'POST',
+                headers:{
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify({
+                    product,
+                })
+            })
+            if(response.ok){
+                alert('product added successfully');
+                setProducts([...products, product]);
+                setNextId((prevId) => {
+                    console.log("Updated id:", prevId + 1);
+                    return prevId + 1;
+                });
+            }
+        } catch(error){
+            console.error('Someting went wrong', error)
+        }
     }
+
+    useEffect(()=>{
+        console.log("useEffect is running");
+        getExpenses();
+    },[])
     return(
-        <form className="expense-form">
+        <form className="expense-form" onSubmit={ExpenseProductListHandler}>
             <label htmlFor="description">Description</label>
             <input type="text" id="description" value={description} onChange={descriptionHandler} placeholder="Describe here"/>
             <label htmlFor="amount">Amount</label>
             <input type="text" id="amount" value={amount} onChange={amountHandler} placeholder="Enter amount"/>
             <label htmlFor="category">Category</label>
             <input type="text" id="category" value={category} onChange={categoryHandler} placeholder="Describe here"/>
-            <button onClick={ExpenseProductListHandler}>Add Expense</button>
+            <button type="submit">Add Expense</button>
             <ExpenseList products={products}/>
         </form>
     )
--- src/Component/Context/Expenses/ExpenseList.js
+++ src/Component/Context/Expenses/ExpenseList.js
@@ -1,20 +1,26 @@
 import React from "react";
-import './ExpenseList.css'
-const ExpenseList = (props) =>{
-    return(
+import './ExpenseList.css';
+
+const ExpenseList = (props) => {
+    return (
         <div className="list">
             <ol className="order">
-                {props.products.map((product)=>(
-                 <div>
-                    <li className="listitems">
-                       <span className="des">{product.description}</span> 
-                       <span className="am">{product.amount}</span>
-                       <span className="cat">{product.category}</span> 
-                    </li>
-                 </div>
+                {props.products.map((product) => (
+                    <div key={product.id} className="listitems">
+                        <li key={`description_${product.id}`}>
+                            <span className="des">{product.description}</span>
+                        </li>
+                        <li key={`amount_${product.id}`}>
+                            <span className="am">{product.amount}</span>
+                        </li>
+                        <li key={`category_${product.id}`}>
+                            <span className="cat">{product.category}</span>
+                        </li>
+                    </div>
                 ))}
             </ol>
         </div>
-    )
+    );
 }
+
 export default ExpenseList;
