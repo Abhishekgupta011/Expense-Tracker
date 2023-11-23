e5217c519d6f9f6c08ea7af82586446a098cbe0b
Abhishekgupta011 <ag25061999@gmail.com>
2023-11-23 20:55:07 +0530

    get request

--- src/Component/Context/Expenses/ExpenseForm.js
+++ src/Component/Context/Expenses/ExpenseForm.js
@@ -1,91 +1,107 @@
 import React, { useEffect, useState } from "react";
 import ExpenseList from "./ExpenseList";
-import './ExpenseForm.css'
+import './ExpenseForm.css';
 
-const ExpenseForm = ()=>{
-    const [description , setDescription] = useState("")
-    const [amount , setAmount] = useState('');
-    const [category , setCategory] = useState("");
+
+const ExpenseForm = () => {
+    const [description, setDescription] = useState("");
+    const [amount, setAmount] = useState('');
+    const [category, setCategory] = useState("");
     const [products, setProducts] = useState([]);
-    const [id , setNextId] = useState(1)
-    const descriptionHandler = (event)=>{
-        setDescription(event.target.value)
+    
+    const descriptionHandler = (event) => {
+        setDescription(event.target.value);
     }
-    const amountHandler = (event)=>{
-        setAmount(event.target.value)
+
+    const amountHandler = (event) => {
+        setAmount(event.target.value);
     }
-    const categoryHandler = (event)=>{
-        setCategory(event.target.value)
+
+    const categoryHandler = (event) => {
+        setCategory(event.target.value);
     }
+
     const getExpenses = async () => {
         try {
-            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json",{
+            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json", {
                 method: 'GET',
                 headers: {
                     "Content-Type": "application/json",
-                  },
+                },
             });
 
             if (response.ok) {
                 const data = await response.json();
-                console.log("data" ,data)
+                console.log(data)
                 const expensesArray = Object.values(data);
-                console.log("expenses", expensesArray)
                 setProducts(expensesArray);
+                // console.log("expensesArray" , expensesArray)
+                // console.log("products" , products)
             }
         } catch (error) {
             console.error('Error fetching expenses:', error);
         }
     }
-    const ExpenseProductListHandler = async(event)=>{
+
+    const generateUniqueId = () => {
+        return Date.now(); // Using timestamp as a simple way to generate a unique ID
+    }
+
+    const ExpenseProductListHandler = async (event) => {
         event.preventDefault();
-        const product = {
-            id: id,
-            description,
-            amount,
-            category,
-        }
-        console.log(id)
         
-        try{
-            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json",{
+
+        try {
+            const response = await fetch("https://expensetracker-e9a1b-default-rtdb.firebaseio.com/expenses.json", {
                 method: 'POST',
-                headers:{
-                    'Content-Type' : 'application/json'
+                headers: {
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    product,
+                    
+                        id: generateUniqueId(), // Set a unique ID using the timestamp
+                        description,
+                        amount,
+                        category,
+                    
                 })
-            })
-            if(response.ok){
-                alert('product added successfully');
-                setProducts([...products, product]);
-                setNextId((prevId) => {
-                    console.log("Updated id:", prevId + 1);
-                    return prevId + 1;
-                });
+            });
+
+            if (response.ok) {
+
+                alert('Product added successfully');
+                setProducts([...products, {id: generateUniqueId(), // Set a unique ID using the timestamp
+                description,
+                amount,
+                category,}]);
+                
             }
-        } catch(error){
-            console.error('Someting went wrong', error)
+        } catch (error) {
+            console.error('Something went wrong', error);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("useEffect is running");
         getExpenses();
-    },[])
-    return(
+    }, []);
+
+    // useEffect(()=>{
+    //     console.log("useffect product" , products)
+    // })
+
+    return (
         <form className="expense-form" onSubmit={ExpenseProductListHandler}>
             <label htmlFor="description">Description</label>
-            <input type="text" id="description" value={description} onChange={descriptionHandler} placeholder="Describe here"/>
+            <input type="text" id="description" value={description} onChange={descriptionHandler} placeholder="Describe here" />
             <label htmlFor="amount">Amount</label>
-            <input type="text" id="amount" value={amount} onChange={amountHandler} placeholder="Enter amount"/>
+            <input type="text" id="amount" value={amount} onChange={amountHandler} placeholder="Enter amount" />
             <label htmlFor="category">Category</label>
-            <input type="text" id="category" value={category} onChange={categoryHandler} placeholder="Describe here"/>
+            <input type="text" id="category" value={category} onChange={categoryHandler} placeholder="Describe here" />
             <button type="submit">Add Expense</button>
-            <ExpenseList products={products}/>
+            <ExpenseList products={products} />
         </form>
-    )
+    );
 }
 
 export default ExpenseForm;
--- src/Component/Context/Expenses/ExpenseList.js
+++ src/Component/Context/Expenses/ExpenseList.js
@@ -2,22 +2,20 @@ import React from "react";
 import './ExpenseList.css';
 
 const ExpenseList = (props) => {
+  
     return (
         <div className="list">
             <ol className="order">
-                {props.products.map((product) => (
-                    <div key={product.id} className="listitems">
-                        <li key={`description_${product.id}`}>
+                {props.products.map((product) =>  {
+    
+                    return (
+                        <li className="listitems" key={product.id}>
                             <span className="des">{product.description}</span>
-                        </li>
-                        <li key={`amount_${product.id}`}>
                             <span className="am">{product.amount}</span>
-                        </li>
-                        <li key={`category_${product.id}`}>
                             <span className="cat">{product.category}</span>
                         </li>
-                    </div>
-                ))}
+                    );
+                })}
             </ol>
         </div>
     );
